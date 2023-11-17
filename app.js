1f2cabaf7fbb5b4aba47e2972572bdacb905e108
Latorreander <latorreander@gmail.com>
2023-11-16 21:11:10 -0300

    'foco no campo e inserção de lista'

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const referenceInput = document.getElementById("reference");
 const quantityInput = document.getElementById("quantity");
 const saveBtn = document.querySelector(".save-btn");
 const panelRef = document.querySelector(".reference-panel");
-const refContent = document.querySelector(".ref");
+const ulRef = document.querySelector(".ul");
 const totalInfo = document.querySelector(".total");
 const skuInfo = document.querySelector(".total-sku");
 const refInfo = document.querySelector(".total-ref");
 const removeBtn = document.querySelector(".remove-btn");
+
+
 
 const arrReferences = [];
 let totalSku = 0;
@@ -20,13 +22,18 @@ document.addEventListener("keypress", (e) => {
 
 window.addEventListener('beforeunload', (e) => {
     e.preventDefault()
+    e.returnValue = '';
 
 })
+
+
 
 
 removeBtn.disabled = true;
 
 saveBtn.addEventListener("click", () => {
+
+    referenceInput.focus()
     
     panelRef.classList.remove("hidden");
 
@@ -78,15 +85,80 @@ saveBtn.addEventListener("click", () => {
         (totalQuantity, value) => totalQuantity + value.Quantity,
         0
     );
+     
 
-    arrReferences.forEach((item) => {
-        refContent.innerHTML = `<span><strong>${item.Reference}  »»»»  ${item.Quantity}Pçs</strong></span>`;
-    });
+ulRef.innerHTML += `<li><strong>${reference.Reference}  »»»»  ${reference.Quantity}Pçs</strong></li>`;
+console.log(ulRef)    // arrReferences.forEach((item) => {
+    //     ulRef.innerHTML += `<li><strong>${item.Reference}  »»»»  ${item.Quantity}Pçs</strong></li>`;
+    // });
 
     totalInfo.innerHTML = `<span>TOTAL: ${totalQuantity}</span>`;
     skuInfo.innerHTML = `<span>SKU: ${totalSku}</span>`;
-    refInfo.innerHTML = `<span>REF: ${arrReferences.length}</span>`;
+    refInfo.innerHTML = `<span>OP: ${arrReferences.length}</span>`;
+
+    
 });
+
+// removeBtn.addEventListener('click', () => {
+
+//     const totalQuantity = arrReferences.reduce(
+//         (totalQuantity, value) => totalQuantity + value.Quantity,
+//         0
+//     );
+  
+//     lastRef = arrReferences[arrReferences.length -1]
+
+    
+//     differenceQuantity = totalQuantity - lastRef.Quantity
+    
+//     console.log(differenceQuantity)
+//     arrReferences.pop()
+
+
+//     // for (let i = 0; i < arrReferences.length; i++) {
+//     //     const ref = arrReferences[i];
+//     //     console.log(ref)
+        
+//     //     if(lastRef.Reference === ref.Reference){
+//     //         totalSku = totalSku
+//     //     }else {
+//     //         totalSku -=1
+//     //     }
+       
+  
+//     // }
+
+
+//     arrReferences.forEach((ref) => {
+
+
+//         if(lastRef.Reference === ref.Reference){
+//         console.log(' já existe a referencia no array')
+//         totalSku = totalSku
+
+//         }else{
+//         console.log('não existe')
+//         totalSku -1
+//         }
+    
+//     })
+
+
+//     if(arrReferences.length === 0){
+//         totalSku = 0
+//         panelRef.classList.add("hidden");
+
+//     }
+
+//     // ulRef.innerHTML = `<li><strong>${lastRef.Reference}  »»»»  ${lastRef.Quantity}Pçs</strong></li>`;
+
+//     totalInfo.innerHTML = `<span>TOTAL: ${differenceQuantity}</span>`;
+//     skuInfo.innerHTML = `<span>SKU: ${totalSku}</span>`;
+//     refInfo.innerHTML = `<span>OP: ${arrReferences.length}</span>`;
+   
+
+// })
+
 
 
 
@@ -123,7 +195,7 @@ saveBtn.addEventListener("click", () => {
 //     }
 
 //     arrReferences.forEach((item) => {
-//         refContent.innerHTML = `<span>${item.Reference} »»»» ${item.Quantity}</span>`;
+//         ulRef.innerHTML = `<span>${item.Reference} »»»» ${item.Quantity}</span>`;
 //     });
 
 //     const totalQuantity = arrReferences.reduce(
@@ -137,7 +209,7 @@ saveBtn.addEventListener("click", () => {
 
 //     if (totalQuantity === 0) {
 //         panelRef.classList.add("hidden");
-//         refContent.innerHTML = "";
+//         ulRef.innerHTML = "";
 //         totalInfo.innerHTML = "";
 
 //         skuInfo.innerHTML = '';
